b275f7bba381b7a37ada252ceff3930acf424dd1
TazerVN <157727133+TazerVN@users.noreply.github.com>
2024-12-22 19:11:19 +0700

    Revert everything
    This reverts commit 8c0e69fe685dc053d5c00c0b8cd88cf0420e735c.

--- src/addtodoButton.js
+++
@@ -1,8 +0,0 @@
-function newtoDoButton(card){
-    const toDoButton = document.createElement("button")
-    toDoButton.textContent = "New To-do"
-    card.appendChild(toDoButton)
-
-}
-
-export {newtoDoButton}
